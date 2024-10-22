f83cd6c5d5d497cd06ee7b8b9619db9520cea62f
yuuhikaze <yuuhikaze@protonmail.com>
2024-10-22 14:24:36 -0500

    feat: creates checkerboard

--- build.sh
+++ build.sh
@@ -3,6 +3,45 @@
 set -uo pipefail
 IFS=$'\n'
 
-mkdir build
-cmake -DCMAKE_PREFIX_PATH=/home/user/Tools/software/Slint-cpp-1.8.0-Linux-x86_64 -B build
-cmake --build build
+init() {
+    mkdir -p build/packages
+}
+
+build() {
+    cmake -DCMAKE_PREFIX_PATH=/home/user/Tools/software/Slint-cpp-1.8.0-Linux-x86_64 -B build
+    cmake --build build
+}
+
+package() {
+    build
+    cpack --config build/CPackConfig.cmake
+}
+
+documentation() {
+    cat <<'EOF'
+OPTIONS
+    -h: Displays documentation
+    -b: Create build
+    -p: Package app
+EOF
+}
+
+while getopts ':hbp' flags; do
+    case "$flags" in
+    b)
+        init
+        build
+        ;;
+    p)
+        init
+        package
+        ;;
+    h) documentation ;;
+    *)
+        echo -e "Unrecognized option\nTry \`build.sh -h\` for more information"
+        exit
+        ;;
+    esac
+done
+
+[ -z "$*" ] && echo -e "Unrecognized option\nTry \`build.sh -h\` for more information"
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,6 @@ int main(int argc, char **argv)
 {
     auto ui = AppWindow::create();
 
-    ui->on_request_increase_value([&]{
-        ui->set_counter(ui->get_counter() + 1);
-    });
-
     ui->run();
     return 0;
 }
