c8d9c7c1d1d96fff63392a364771a7653791c01a
yuuhikaze <yuuhikaze@protonmail.com>
2024-11-05 08:16:54 -0500

    feat: splits interface into components

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "app-window.h"
+#include "slint.h"
 
 int main(int argc, char **argv)
 {
     auto ui = AppWindow::create();
+    // ui->set_maximized(true);
+    // slint::Window::
 
     ui->run();
     return 0;
