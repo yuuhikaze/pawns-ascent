2c15dc9ac12569549f5a026a22de5087e3085728
yuuhikaze <yuuhikaze@protonmail.com>
2024-11-05 12:54:31 -0500

    feat: adds player banners

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char **argv)
     auto ui = AppWindow::create();
     // ui->set_maximized(true);
     // slint::Window::
+    ui->window().set_maximized(true);
 
     ui->run();
     return 0;
